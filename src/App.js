007b3a48952794ebf32c83892cdbc264e27b6234
Mateusz <mateuszrustowiczyt@gmail.com>
2023-07-08 16:51:25 +0200

    added lazy

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ import { useState } from 'react';
 import SignUp from 'pages/sign-up';
 
 function App() {
-  const [isLoading,setIsLoading] = useState(true);
   return (
     <div className="App">
       <Routes>
-        <Route path="/" exact element={<Home isLoading={isLoading} setIsLoading={setIsLoading}/>}></Route>
-        <Route path="/pricing" exact element={<Pricing  isLoading={isLoading} setIsLoading={setIsLoading}/>}></Route>
-        <Route path="/exchange" exact element={<Exchange  isLoading={isLoading} setIsLoading={setIsLoading}/>}></Route>
-        <Route path="/wallet" exact element={<Wallet  isLoading={isLoading} setIsLoading={setIsLoading}/>}></Route>
+        <Route path="/" exact element={<Home/>}></Route>
+        <Route path="/pricing" exact element={<Pricing/>}></Route>
+        <Route path="/exchange" exact element={<Exchange/>}></Route>
+        <Route path="/wallet" exact element={<Wallet/>}></Route>
         <Route path="/sign-up" exact element={<SignUp/>}/>
       </Routes>
     </div>
