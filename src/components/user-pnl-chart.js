889c5b0f250f26e6f28552fc2fe5ac0ee3419f42
Mateusz <mateuszrustowiczyt@gmail.com>
2023-07-17 14:52:32 +0200

    fixed dashboard appearance

--- src/components/user-pnl-chart.js
+++ src/components/user-pnl-chart.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { DashboardBox } from "./dashboard-box";
 import { Bar} from 'react-chartjs-2';
 import accountData from 'utils/account-data.json'
@@ -6,7 +7,13 @@ import { monthPnlData } from "utils/options";
 
 
 export function PnlChart() {
-
+    const [id,setId] = useState(0);
+    
+    useEffect(() => {
+        window.addEventListener("resize",() => {
+            setId(id => id + 1);
+        })
+    },[setId]);
     return (
         <DashboardBox boxHeading="pnl based on month interval" boxName="pnl-month-chart">
             <Bar options={chartOptions} data={monthPnlData} />
